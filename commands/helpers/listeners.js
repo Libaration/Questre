5b402371e2e3cc53429c33c61028f985deb19860
Cristian Deleon <libaration@gmail.com>
2021-09-06 05:56:56 -0400

    adds titlecasing for song / notifcation to begin search

--- commands/helpers/listeners.js
+++ commands/helpers/listeners.js
@@ -1,11 +1,26 @@
+import {
+  bold,
+  italic,
+  strikethrough,
+  underscore,
+  spoiler,
+  quote,
+  blockQuote,
+} from '@discordjs/builders';
 export const interactionListener = async (interaction) => {
   if (!interaction.isCommand()) return; //if the interaction isn't a registered command do nothing
   const { commandName } = interaction; //ES6 Destructuring to "rename" interaction to commandName for cleaner code
   switch (commandName) {
-    case 'user':
+    case 'play':
       try {
-        interaction.reply('pong');
-        console.log(interaction.options.getUser('user'));
+        const song = interaction.options.getString('song').toLowerCase();
+        const titled = song
+          .split(' ')
+          .map((s) => {
+            return s.charAt(0).toUpperCase() + s.slice(1);
+          })
+          .join(' ');
+        interaction.reply(`Searching for ${italic(titled)}`);
       } catch {
         console.log('failed to reply to interaction');
       }
