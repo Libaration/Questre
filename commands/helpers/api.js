416b555016ac69309da6822db2fdfc6c82acd740
Cristian Deleon <libaration@gmail.com>
2021-09-06 20:47:59 -0400

    returns list of searches using fetch to youtube api

--- commands/helpers/api.js
+++ commands/helpers/api.js
@@ -1,7 +1,7 @@
-const fetch = (...args) =>
-  import('node-fetch').then(({ default: fetch }) => fetch(...args));
+import fetch from 'node-fetch';
 export const fetchSearchResults = async (query) => {
   const URL = `https://youtube.googleapis.com/youtube/v3/search?part=snippet&q=${query}&safeSearch=none&key=${process.env.YTKEY}`;
   const response = await fetch(URL);
-  console.log(response);
+  const responseJSON = await response.json();
+  console.log(responseJSON);
 };
